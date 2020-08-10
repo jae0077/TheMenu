1043ca44fa08c2655ff7afe3455c59029cb0a1c9
Coblin_ <sjj0077@naver.com>
2020-08-10 15:08:20 +0900

    shopdetail_revise/views 수정

--- api/shopdetail_revise/views.py
+++ api/shopdetail_revise/views.py
@@ -3,27 +3,24 @@ from django.http import HttpResponse
 from django.sqlSetting import settings
 import pymysql
 import json
-import qrcode
-import random
-import hashlib
 
 def handler(request):
-    usertbl_idx = int(request.POST["_usertbl_idx"])
+    token = request.POST["_token"]
     shop_name = request.POST["_shop_name"]
     shop_menu = request.POST["_shop_menu"]
     shop_tel = request.POST["_shop_tel"]
     shop_location = request.POST["_shop_location"]
     shop_qrlink = request.POST["_qm_qr_link"]
-
+    
     db = pymysql.connect(host=settings.RDB_HOST, port=settings.RDB_PORT, user=settings.RDB_ID, passwd=settings.RDB_PW, db=settings.RDB_DBNAME)
 
     cursor = db.cursor()
-    cursor.execute("CALL qm_shopdetail_revise_pd(%d, '%s', '%s', '%s', '%s', '%s');" %(usertbl_idx, shop_name, shop_menu, shop_tel, shop_location, shop_qrlink))
+    cursor.execute("CALL qm_shopdetail_revise_pd(%s, '%s', '%s', '%s', '%s', '%s');" %(token, shop_name, shop_menu, shop_tel, shop_location, shop_qrlink))
 
     fetchResult = cursor.fetchone()
     db.commit()
     db.close()
-    result = json.dumps(fetchResult)
+    result = fetchResult
 
-    return HttpResponse(result, content_type='application/json')
+    return HttpResponse(result)
 
